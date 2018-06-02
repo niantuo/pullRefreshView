de4feb6a7399f92ad4b771655b1c7ae43c02928e
niantuo <976056042@qq.com>
2018-06-02 15:19:58 +0800

    '更新退出再次访问接口'

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
+import GiftedListView from './lib/GiftedListView'
 
 AppRegistry.registerComponent('pullRefreshView', () => App);
+
+
+export default GiftedListView
